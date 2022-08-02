e1d6849a345faf49caaffe1b1688c40df1854c46
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-02 15:54:36 -0300

    requisitos 4 e 5

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let index = 1; index <= 25; index += 1) {
+    const divColor = document.createElement('div');
+    divColor.className = 'pixel';
+    document.getElementById('pixel-board').appendChild(divColor);
+  }
+
+
