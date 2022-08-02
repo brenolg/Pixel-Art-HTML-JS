e821de743107f0f0d931d4a77ddfa18bdd60a1d7
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-02 20:09:18 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -4,4 +4,23 @@ for (let index = 1; index <= 25; index += 1) {
     document.getElementById('pixel-board').appendChild(divColor);
   }
 
+let corSelecionada = "black";
+
+window.onload = function corInicial (){
+     let preto = document.querySelector('#cor1');
+     preto.classList.add('selected')
+     console.log(preto)
+}
+
+let board = document.querySelector('#color-palette');
+board.addEventListener('click', function(event) {
+     let divSelecionada =  event.target;
+     let selected = document.querySelector('.selected')
+     selected.classList.remove('selected')
+     divSelecionada.classList.add('selected')
+   });
+
+
+
+
 
