c93421d99af14df5a660aa833172ec8eff93deed
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-04 15:09:35 -0300

    requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-for (let index = 1; index <= 25; index += 1) {
-    const divColor = document.createElement('div');
-    divColor.className = 'pixel';
-    document.getElementById('pixel-board').appendChild(divColor);
-  }
-
-let corSelecionada = "black";
-
 window.onload = function corInicial (){
-     let preto = document.querySelector('#cor1');
-     preto.classList.add('selected')
+  let preto = document.querySelector('#cor1');
+  preto.classList.add('selected')
 
+  for (let index = 1; index <= 25; index += 1) {
+ let divColor = document.createElement('div');
+ divColor.className = 'pixel';
+ document.getElementById('pixel-board').appendChild(divColor);
 }
+}
+
+let input = document.querySelector('#board-size');
+let botaoPopulate = document.querySelector('#generate-board');
+let divPai = document.querySelector('#pixel-board');
+
+botaoPopulate.addEventListener('click', function(){
+    if((input.value >= 5) && (input.value <= 50)){
+      let pixelDel = document.querySelectorAll('.pixel');
+      for (let index = 0; index < pixelDel.length; index += 1) {
+        pixelDel[index].remove();
+        
+      }
+      for (let index = 1; index <= input.value; index += 1) {
+         divColor = document.createElement('div');
+         divColor.className = 'pixel';
+         document.getElementById('pixel-board').appendChild(divColor);
+      }
+
+      } else {
+      alert("Board inválido!");
+    }
+})
 
 let board = document.querySelector('#color-palette');
 board.addEventListener('click', function cor(event) {
@@ -30,6 +49,14 @@ pixel.addEventListener('click', function (e){
 
 });
 
+let pixelCler= document.querySelectorAll('.pixel');
+let botao = document.querySelector('#clear-board');
+botao.addEventListener('click', function(){
+  for (let index = 0; index <= pixelCler.length; index += 1) {
+    pixelCler[index].style.backgroundColor = "white";
+    
+  }
+})
 
 
 
