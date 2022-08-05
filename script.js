5b50fbaeaa298f7f9f565c2394392757ac467120
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-05 20:06:38 -0300

    ultimo requerimento

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 window.onload = function corInicial (){
   let preto = document.querySelector('#cor1');
   preto.classList.add('selected')
+  let cor2 = document.querySelector('#cor2');
+  let cor3 = document.querySelector('#cor3');
+  let cor4 = document.querySelector('#cor4');
+  cor2.style.backgroundColor = criarCor()
+  cor3.style.backgroundColor = criarCor()
+  cor4.style.backgroundColor = criarCor()
 
   for (let index = 1; index <= 25; index += 1) {
  let divColor = document.createElement('div');
  divColor.className = 'pixel';
  document.getElementById('pixel-board').appendChild(divColor);
 }
+}
+
+function criarCor () {
+  let r = Math.floor(Math.random()*255);
+  let g = Math.floor(Math.random()*255);
+  let b = Math.floor(Math.random()*255);
+  let cor = 'rgb('+r+','+g+','+b+')';
+  return cor;
 }
 
 let input = document.querySelector('#board-size');
